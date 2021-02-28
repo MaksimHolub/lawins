4ffa65f6cba4f51f484aba9aaf8af8b3800f3bb9
MaksimHolub <maksim.holub@gmail.com>
2021-03-01 00:48:23 +0300

    Added 1-7, 15 section

---
+++ app/js/main.js
@@ -0,0 +1,18 @@
+$(function () {
+
+	$('.specialist-avatars__top-item').on('click', function (e) {
+		e.preventDefault();
+		$('.specialist-avatars__top-item').removeClass('specialist-avatars__top-item--active');
+		$(this).addClass('specialist-avatars__top-item--active');
+		$('.specialist-avatars__content-item').removeClass('specialist-avatars__content-item--active');
+		$($(this).attr('href')).addClass('specialist-avatars__content-item--active');
+
+	})
+
+		$('.support-content__details').on('click', function (e) {
+		e.preventDefault();
+		$(this).toggleClass('support-content__details--open');
+
+	})
+
+});
